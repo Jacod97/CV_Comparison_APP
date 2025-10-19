12ea1b6aca50a86f6ea20d178a802811c0fd0af4
Jacod97 <wotlr6894@naver.com>
2025-10-19 17:04:57 +0900

    set

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QLabel, QHBoxLayout
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QLabel, QHBoxLayout, QListWidget
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
 
@@ -8,43 +8,73 @@ from PyQt5.QtCore import Qt
 class MainWindow(QMainWindow):
     def __init__(self) -> None:
         super().__init__()
-        self._init_ui()
+        self.initUI()
 
-    def _init_ui(self) -> None:
+    def initUI(self) -> None:
         self.setWindowTitle("Image Comparison Viewer")
         self.resize(1600, 800)
 
-        # self._create_menubar("test")
-        self._setup_central_widget()
+        # 전체 레이아웃 구성
+        central_widget = QWidget(self)                         
+        self.setCentralWidget(central_widget)                   
+        self.main_layout = QHBoxLayout(central_widget)               
+
+        self.sidebar = self.set_sidebar_widget()                
+        self.main_layout.addWidget(self.sidebar)                     
+
+        # 이미지 시각화
+        self.img_container = QWidget()                      
+        self.main_layout.addWidget(self.img_container)
+        self.set_img_widget()
 
     # 메뉴바 필요하면 주석풀고 만들기
     # def _create_menubar(self,menu_name) -> None:
     #     menubar = self.menuBar()
     #     menubar.setNativeMenuBar(False)
     #     menubar.addMenu(menu_name)
+    
+    def set_sidebar_widget(self):                       
+        sidebar = QListWidget()
+        sidebar.addItems(find_common_images(IMG_PATH))
+        sidebar.itemClicked.connect(self._on_sidebar_clicked)  
+        return sidebar
 
-    def _setup_central_widget(self) -> None:
-        central_widget = QWidget(self)
-        layout = QVBoxLayout(central_widget)
-        self.setCentralWidget(central_widget)
+    def _on_sidebar_clicked(self, item):                    
+        filename = item.text()
+        # print(f"[INFO] 선택된 이미지: {filename}")
+        self.set_img_widget(selected_name=filename) 
 
+    def set_img_widget(self, selected_name=None):
+        # 기존 레이아웃 가져오기
+        layout = self.img_container.layout()
+
+        # 기존 위젯 정리
+        while layout.count():
+            item = layout.takeAt(0)
+            widget = item.widget()
+            if widget is not None:
+                widget.deleteLater()
+
+        # 공통 이미지 목록과 대상 파일명 선택
         common_images = find_common_images(IMG_PATH)
-        first_image = common_images[0]
+        img_fname = selected_name if selected_name else common_images[0]
 
+        # 수평 레이아웃 생성
         hbox = QHBoxLayout()
-        
         layout.addLayout(hbox)
 
+        # 이미지 3개 표시
         for key, path in IMG_PATH.items():
-            img, caption = self.show_img(path, first_image, key)
+            img, caption = self.show_img(path, img_fname, key)
 
             vbox = QVBoxLayout()
             vbox.addWidget(img)
             vbox.addWidget(caption)
 
             container = QWidget()
-            container.setLayout(vbox)     
-            hbox.addWidget(container) 
+            container.setLayout(vbox)
+            hbox.addWidget(container)
+
  
     def show_img(self, path, img_name, caption_text):
         img_path = os.path.join(path, img_name)
